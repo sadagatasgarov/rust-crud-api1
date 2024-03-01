608b8a4da24f5d52c0e043d29d329b7671cb1f49
sadagatasgarov <sadagat.asgarov@gmail.com>
2024-03-01 01:23:48 -0800

    basic get post islerini hell etdik

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 use rocket::serde::json::{json, Value};
 
 // #[get{"/"}]
@@ -6,16 +7,88 @@ use rocket::serde::json::{json, Value};
 //     "Hello world!\n"
 // }
 
-#[get{"/"}]
+#[get("/")]
 fn hello() -> Value {
     json!("Hello, world!")
 }
+
+#[get("/baza")]
+fn get_baza() -> Value {
+    json!([
+        {
+            "id": 1,
+            "name": "Sada Asga"
+        },
+
+        {
+            "id": 2,
+            "name": "Sakh Asga"
+        }
+    ])
+}
+
+#[get("/baza/<id>")]
+fn view_baza(id: i32) -> Value {
+    json!([
+        {
+            "id": id,
+            "name": "Sada Asga",
+            "email": "sadagatasgarov@gmil.com"
+        }
+    ])
+}
+
+#[post("/baza", format = "json")]
+fn create_baza() -> Value {
+    json!([
+        {
+            "id": 3,
+            "name": "Sada2 Asga2",
+            "email": "sadagatasgarov@gmil.com"
+        }
+    ])
+}
+
+
+#[put("/baza/<id>", format="json")]
+fn update_baza(id: i32) -> Value {
+    json!([
+        {
+            "id": id,
+            "name": "Sada2 Asga2",
+            "email": "sadagatasgarov@gmil.com"
+        }
+    ])
+}
+
+
+#[delete("/baza/<id>")]
+fn delete_baza(id:i32) -> Value {
+    json!([
+        {
+            "id": id,
+            "name": "Sada2 Asga2",
+            "email": "sadagatasgarov@gmil.com"
+        }
+    ])
+}
+
+
+
+
 
 
 #[rocket::main]
 async fn main() {
     let _ = rocket::build()
-        .mount("/", routes![hello])
+        .mount("/", routes![
+            hello,
+            get_baza,
+            view_baza,
+            create_baza,
+            update_baza,
+            delete_baza
+            ])
         .launch()
         .await;
 }
