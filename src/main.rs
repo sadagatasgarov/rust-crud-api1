f7d056ba7c600caf132407922e4bbb9a68dcc87c
sadagatasgarov <sadagat.asgarov@gmail.com>
2024-04-16 22:20:14 +0400

    Auth ile elaqeli olan kodlari auth.rs icine qoyduq

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,14 @@
 #[macro_use]
 extern crate rocket;
-use rocket::{request::Outcome, http::Status, request::FromRequest, serde::json::{json, Value}, Request};
-//use rocket::response::status;
 
-pub struct BasicAuth {
-    pub username: String,
-    pub password: String,
-}
+mod auth;
 
-impl BasicAuth {
-    fn from_authorization_header(header: &str) -> Option<BasicAuth> {
-        let split = header.split_whitespace().collect::<Vec<_>>();
-        if split.len() != 2 {
-            return None;
-        }
+use auth::BasicAuth;
 
-        if split[0] != "Basic" {
-            return None;
-        }
-
-        Self::from_base64_encoded(split[1])
-    }
-
-    fn from_base64_encoded(base64_string: &str) -> Option<BasicAuth> {
-        let decoded = base64::decode(base64_string).ok()?;
-        let decoded_str = String::from_utf8(decoded).ok()?;
-        let split = decoded_str.split(":").collect::<Vec<_>>();
-
-        if split.len() !=2 {
-            return None;
-        }
-
-        let (username, password) = (split[0].to_string(), split[1].to_string());
-
-        Some(BasicAuth{
-            username,
-            password
-        })
-
-
-    }
-}
-
-#[rocket::async_trait]
-impl<'r> FromRequest<'r> for BasicAuth {
-    type Error = ();
-    
-    async fn from_request(request: &'r Request<'_>) -> Outcome<Self, Self::Error> {
-        let auth_header = request.headers().get_one("Authorization");
-        if let Some(auth_header) = auth_header {
-            if let Some(auth) = Self::from_authorization_header(auth_header){
-                return Outcome::Success(auth);
-            }
-        }
-
-        Outcome::Error((Status::Unauthorized, ()))
-    }
-}
+use rocket::serde::json::{json, Value};
+/*
+use rocket::response::status;
+*/
 
 #[get("/")]
 fn hello() -> Value {
