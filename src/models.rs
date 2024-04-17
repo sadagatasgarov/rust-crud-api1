3c5e5fae9a050c654d246b90b5eef226d42837eb
sadagatasgarov <sadagat.asgarov@gmail.com>
2024-04-18 00:58:03 +0400

    id ve data yerleri put zamani deyisirdi serde(skip_deserializing)

--- src/models.rs
+++ src/models.rs
@@ -2,9 +2,11 @@ use super::schema::baza;
 
 #[derive(serde::Serialize, serde::Deserialize, Queryable)]
 pub struct Baza {
+    #[serde(skip_deserializing)]
     pub id: i32,
     pub name: String,
     pub email: String,
+    #[serde(skip_deserializing)]
     pub created_at: String,
 }
 
